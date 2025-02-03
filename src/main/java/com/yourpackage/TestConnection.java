2fce618df4e5603d63723e20adce887784a49be7
Tom Ho <82748618+tomho-git@users.noreply.github.com>
2025-02-03 14:41:31 -0500

    issue resolved: connection closed before really connect

---
+++ src/main/java/com/yourpackage/TestConnection.java
@@ -0,0 +1,29 @@
+package com.yourpackage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "shingchunho";
+        String password = ""; // Replace with your actual password
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+                Statement stmt =  connection.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT NOW()");
+                rs.next();
+                System.out.println(rs.getString(1));
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/yourpackage/repository/DatabaseRepository.java
+++ src/main/java/com/yourpackage/repository/DatabaseRepository.java
@@ -9,14 +9,19 @@ import java.sql.Statement;
 
 @Repository
 public class DatabaseRepository {
+    
 
     public String getCurrentTime() {
         String currentTime = null;
+
         try (Connection connection = DatabaseConnection.getConnection()) {
-            Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT NOW()");
-            if (rs.next()) {
-                currentTime = rs.getString(1);
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+                Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT NOW()");
+                if (rs.next()) {
+                    currentTime = rs.getString(1);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
